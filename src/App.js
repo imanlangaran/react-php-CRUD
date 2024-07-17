c83e3d8a1e38f77a176befb90b9cf9a17c424199
Iman Langaran <imanlangaran@gmail.com>
2024-07-17 09:57:46 +0330

    initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h5>
+        React CRUD operations using PHP API and MySQL
+      </h5>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListUser.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ListUser() {
+  return (
+    <h1>List users</h1>
+  )
+}
+
+export default ListUser
