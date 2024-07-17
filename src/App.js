65301bea5a777406ca5d86a2a27c74eafdc8ff1d
Iman Langaran <imanlangaran@gmail.com>
2024-07-17 13:05:48 +0330

    implement create user front

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import './App.css';
+import ListUser from "./components/ListUser";
+import CreateUser from "./components/CreateUser";
+import EditUser from "./components/EditUser";
 
 function App() {
   return (
@@ -7,6 +10,29 @@ function App() {
       <h5>
         React CRUD operations using PHP API and MySQL
       </h5>
+
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                List Users
+              </Link>
+            </li>
+            <li>
+              <Link to="/user/create">
+                Create User
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route path="user/create" element={<CreateUser />} />
+          <Route path="user/:id/edit" element={<EditUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreateUser() {
+
+  const [inputs, setInputs] = useState({
+
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
   return (
-    <div>CreateUser</div>
+    <div>
+      <h1>Create User</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Name:  </label>
+        <input type='text' name='name' onChange={handleChange} />
+        <br />
+        <label>Email: </label>
+        <input type='email' name='email' onChange={handleChange}/>
+        <br />
+        <label>Phone: </label>
+        <input type="tel" name='phone' onChange={handleChange}/>
+        <br />
+        <button>Save</button>
+      </form>
+    </div>
   )
 }
 
